39ab1bc0647588dfbcb5e953869e5197115d5fc9
xxj15 <seojini00@naver.com>
2025-05-07 13:56:52 +0900

    env(custom): commitizen 설정

---
+++ .cz-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  types: [
+    { value: "feat", name: "✨ feat: 새로운 기능 추가 " },
+    { value: "fix", name: "🐛 fix: 버그 수정" },
+    { value: "refactor", name: "⚡️ refactor: 리팩토링 " },
+    { value: "docs", name: "📄 docs: 문서 변경" },
+    { value: "env", name: "🌱 env: 설정 변경" },
+    { value: "chore", name: "🍯 chore: 자잘한 수정" },
+    { value: "style", name: "💄 style: style만 변경하는 경우" },
+    { value: "perf", name: "💯 performance: 성능 향상" },
+  ],
+  subjectLimit: 72,
+};
