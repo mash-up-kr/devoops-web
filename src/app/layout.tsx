b2091ebf12fcf49cc2e03616c27173440ff8aa3a
xxj15 <seojini00@naver.com>
2025-06-04 21:36:34 +0900

    feat: portal 기반 UI 컴포넌트 렌더링을 위한 DOM 노드 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang={'en'}>
-      <body className={`${geistSans.variable}  antialiased`}>{children}</body>
+      <body className={`${geistSans.variable}  antialiased`}>
+        {children}
+        <div id={'portal'} />
+      </body>
     </html>
   );
 }
