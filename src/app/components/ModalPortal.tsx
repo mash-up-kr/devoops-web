51486fb528b1bb032b5fcd10e6dfd294b50ad202
xxj15 <seojini00@naver.com>
2025-06-04 23:39:25 +0900

    feat: document 사용을 위한 ESLint browser env 설정 및 ModalPortal 컴포넌트 구현

---
+++ src/app/components/ModalPortal.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+
+interface ModalPortalProps {
+  children: ReactNode;
+}
+
+function ModalPortal({ children }: ModalPortalProps) {
+  const portalRoot = typeof window !== 'undefined' ? document.getElementById('portal') : null;
+  return portalRoot ? createPortal(children, portalRoot) : null;
+}
+
+export default ModalPortal;
