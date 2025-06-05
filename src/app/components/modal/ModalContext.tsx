5088fb48add214e0c886de3e85d34688166a5bfb
xxj15 <seojini00@naver.com>
2025-06-05 09:32:20 +0900

    feat: 모달 열림/닫힘 상태를 전역에서 관리하기 위한 Modal Context 구현

---
+++ src/app/components/modal/ModalContext.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { createContext, useContext, useReducer, type Dispatch, type ReactNode } from 'react';
+
+type ModalState = {
+  isOpen: boolean;
+};
+
+const initialState: ModalState = {
+  isOpen: false,
+};
+
+type ModalAction = { type: 'OPEN' } | { type: 'CLOSE' };
+type ModalDispatch = Dispatch<ModalAction>;
+
+const ModalStateContext = createContext<ModalState | null>(null);
+const ModalDispatchContext = createContext<ModalDispatch | null>(null);
+
+function modalReducer(state: ModalState, action: ModalAction): ModalState {
+  switch (action.type) {
+    case 'OPEN':
+      return { isOpen: true };
+    case 'CLOSE':
+      return { isOpen: false };
+    default:
+      return state;
+  }
+}
+
+export function ModalProvider({ children }: { children: ReactNode }) {
+  const [state, dispatch] = useReducer(modalReducer, initialState);
+
+  return (
+    <ModalStateContext.Provider value={state}>
+      <ModalDispatchContext.Provider value={dispatch}>{children}</ModalDispatchContext.Provider>
+    </ModalStateContext.Provider>
+  );
+}
+
+export function useModalState() {
+  const context = useContext(ModalStateContext);
+  if (!context) throw new Error('useModalState must be used within a ModalProvider');
+  return context;
+}
+
+export function useModalDispatch() {
+  const context = useContext(ModalDispatchContext);
+  if (!context) throw new Error('useModalDispatch must be used within a ModalProvider');
+  return context;
+}
