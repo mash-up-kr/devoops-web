f8fe8485228d47772052c1dd776f6807f5bd8275
jeongwoo903 <www8565@naver.com>
2025-06-05 18:53:08 +0900

    feat: 정적 자원 도메인 추가
    * `next.config.ts`에 정적 자원 도메인을 추가함

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['static.dev-oops.kr'],
+  },
 };
 
 export default nextConfig;
