9694c9b80a7ce4a53d9d971c0ba07e44ed8f206a
jeongwoo903 <www8565@naver.com>
2025-06-05 20:43:03 +0900

    refactor: 환경에 맞는 `QueryClient` 생성 로직 구현
    * `getQueryClient.tsx`를 추가하여 서버와 브라우저 환경에 맞는 QueryClient 생성 로직을 구현
    
    * `QueryProvider.tsx`에서 QueryClient 인스턴스를 직접 생성하는 대신 `getQueryClient` 함수를 사용하도록 변경

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { PropsWithChildren } from 'react';
 
+import getQueryClient from '@/app/providers/getQueryClient';
+
 export default function QueryProvider({ children }: PropsWithChildren) {
-  const queryClient = new QueryClient();
+  const queryClient = getQueryClient();
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
