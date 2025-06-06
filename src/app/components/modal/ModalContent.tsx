6dacef15982d55fd18f470b58a17296796e92b26
xxj15 <seojini00@naver.com>
2025-06-06 16:38:34 +0900

    feat: 모달 열림 상태에 따라 렌더링되는 ModalContent.tsx 구현

---
+++ src/app/components/modal/ModalContent.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React from 'react';
+
+import { useModalState } from './ModalContext';
+import ModalPortal from './ModalPortal';
+
+interface ModalContentProps {
+  children: React.ReactNode;
+}
+
+function ModalContent({ children }: ModalContentProps) {
+  const { isOpen } = useModalState();
+
+  if (!isOpen) return null;
+
+  return (
+    <ModalPortal>
+      <div className={'fixed inset-0 z-[9995] flex items-center justify-center bg-black/40'}>
+        <div className={'relative rounded-xl bg-white p-6 shadow-xl'}>{children}</div>
+      </div>
+    </ModalPortal>
+  );
+}
+
+export default ModalContent;
