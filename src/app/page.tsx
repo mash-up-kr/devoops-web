aa41d9b043ec4951a9e0f7c06fb3ddb87739bbad
jeongwoo903 <www8565@naver.com>
2025-06-24 20:30:35 +0900

    style: typography 토큰에 대한 유틸리티 클래스 정의

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,50 @@
 export default function Home() {
-  return <div>{'DevOops'}</div>;
+  return (
+    <div className={'grid grid-cols-4 gap-4'}>
+      <span className={'text-h1 font-regular'}>{'DevOops'}</span>
+      <span className={'text-h1 font-medium'}>{'DevOops'}</span>
+      <span className={'text-h1 font-semibold'}>{'DevOops'}</span>
+      <span className={'text-h1 font-bold'}>{'DevOops'}</span>
+
+      <span className={'text-h2 font-regular'}>{'DevOops'}</span>
+      <span className={'text-h2 font-medium'}>{'DevOops'}</span>
+      <span className={'text-h2 font-semibold'}>{'DevOops'}</span>
+      <span className={'text-h2 font-bold'}>{'DevOops'}</span>
+
+      <span className={'text-h3 font-regular'}>{'DevOops'}</span>
+      <span className={'text-h3 font-medium'}>{'DevOops'}</span>
+      <span className={'text-h3 font-semibold'}>{'DevOops'}</span>
+      <span className={'text-h3 font-bold'}>{'DevOops'}</span>
+
+      <span className={'text-h4 font-regular'}>{'DevOops'}</span>
+      <span className={'text-h4 font-medium'}>{'DevOops'}</span>
+      <span className={'text-h4 font-semibold'}>{'DevOops'}</span>
+      <span className={'text-h4 font-bold'}>{'DevOops'}</span>
+
+      <span className={'text-h5 font-regular'}>{'DevOops'}</span>
+      <span className={'text-h5 font-medium'}>{'DevOops'}</span>
+      <span className={'text-h5 font-semibold'}>{'DevOops'}</span>
+      <span className={'text-h5 font-bold'}>{'DevOops'}</span>
+
+      <span className={'text-body-large font-regular'}>{'DevOops'}</span>
+      <span className={'text-body-large font-medium'}>{'DevOops'}</span>
+      <span className={'text-body-large font-semibold'}>{'DevOops'}</span>
+      <span className={'text-body-large font-bold'}>{'DevOops'}</span>
+
+      <span className={'text-body-medium font-regular'}>{'DevOops'}</span>
+      <span className={'text-body-medium font-medium'}>{'DevOops'}</span>
+      <span className={'text-body-medium font-semibold'}>{'DevOops'}</span>
+      <span className={'text-body-medium font-bold'}>{'DevOops'}</span>
+
+      <span className={'text-body-small font-regular'}>{'DevOops'}</span>
+      <span className={'text-body-small font-medium'}>{'DevOops'}</span>
+      <span className={'text-body-small font-semibold'}>{'DevOops'}</span>
+      <span className={'text-body-small font-bold'}>{'DevOops'}</span>
+
+      <span className={'text-caption font-regular'}>{'DevOops'}</span>
+      <span className={'text-caption font-medium'}>{'DevOops'}</span>
+      <span className={'text-caption font-semibold'}>{'DevOops'}</span>
+      <span className={'text-caption font-bold'}>{'DevOops'}</span>
+    </div>
+  );
 }
