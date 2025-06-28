dcdac3706c6077480beabb81d281605352a6aeeb
HajunRyu(류하준) <71176945+HaJunRyu@users.noreply.github.com>
2025-06-29 02:47:43 +0900

    style: 회고 상세 페이지의 최대, 최소 너비를 지정한다 (#25)

--- src/app/retrospective/[pullRequestId]/layout.tsx
+++ src/app/retrospective/[pullRequestId]/layout.tsx
@@ -5,5 +5,5 @@ interface RetrospectiveLayoutProps {
 }
 
 export default function RetrospectiveLayout({ children }: RetrospectiveLayoutProps) {
-  return <div className={'px-[40px] pt-[68px] pb-[132px]'}>{children}</div>;
+  return <div className={'mx-auto max-w-[840px] min-w-[640px] px-[40px] pt-[68px] pb-[132px]'}>{children}</div>;
 }
