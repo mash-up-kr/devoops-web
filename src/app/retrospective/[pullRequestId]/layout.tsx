012cba547bb14fe35c22fde8d5521ae076ab1d68
HaJunRyu <hjr4557@naver.com>
2025-06-28 22:23:35 +0900

    feat: 회고페이지 Header 영역의 UI를 구현한다

---
+++ src/app/retrospective/[pullRequestId]/layout.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function RetrospectiveLayout({ children }: { children: React.ReactNode }) {
+  return <div className={'px-[40px] pt-[68px]'}>{children}</div>;
+}
---
+++ src/app/retrospective/[pullRequestId]/page.tsx
@@ -0,0 +1,10 @@
+import RetrospectiveHeader from '@/components/retrospective/RetrospectiveHeader';
+
+export default function RetrospectivePage() {
+  return (
+    <>
+      <RetrospectiveHeader />
+      <main />
+    </>
+  );
+}
