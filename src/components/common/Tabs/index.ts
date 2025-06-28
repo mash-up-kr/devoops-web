3c1d896beb0b8438159391913a0eb3c8e8fe185b
HaJunRyu <hjr4557@naver.com>
2025-06-28 21:01:41 +0900

    refactor: common component 파일 관리 구조를 변경한다

---
+++ src/components/common/Tabs/index.ts
@@ -0,0 +1,4 @@
+export { default as Tabs } from './Tabs';
+export { default as TabsContent } from './TabsContent';
+export { default as TabsList } from './TabsList';
+export { default as TabsTrigger } from './TabsTrigger';
