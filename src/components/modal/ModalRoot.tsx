3eb15e464900ea185701c83483789cf06568596f
jeongwoo903 <www8565@naver.com>
2025-07-03 00:54:35 +0900

    refactor: 중복되는 `ModalContext` 컴포넌트 제거

--- src/components/modal/ModalRoot.tsx
+++ src/components/modal/ModalRoot.tsx
@@ -1,25 +1,14 @@
 'use client';
 
-import React, { type ReactNode } from 'react';
+import { ReactNode } from 'react';
 
-import { ModalProvider, useModalState } from '@/components/modal/ModalContext';
-import ModalPortal from '@/components/modal/Portal';
+import ModalProvider from '@/components/modal/ModalContext';
 
 interface ModalRootProps {
   children: ReactNode;
   defaultOpen?: boolean;
 }
 
-function ModalContent({ children }: { children: ReactNode }) {
-  const { isOpen } = useModalState();
-
-  if (!isOpen) return null;
-
-  return <ModalPortal>{children}</ModalPortal>;
-}
-
 export default function ModalRoot({ children, defaultOpen }: ModalRootProps) {
   return <ModalProvider defaultOpen={defaultOpen}>{children}</ModalProvider>;
 }
-
-export { ModalContent };
