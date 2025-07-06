bab2b6f819aec2628f54098c87aae24b76f20ea2
jeongwoo903 <www8565@naver.com>
2025-07-07 01:30:01 +0900

    feat: `ProfileModal` 컴포넌트 제작
    * dimmed 영역 click시 modal이 닫히도록 제작

---
+++ src/components/common/Profile/ProfileContent.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+
+import Avatar from '@/assets/images/avatar.png';
+import RepositoryBadge from '@/components/common/RepositoryBadge';
+
+export default function ProfileContent() {
+  return (
+    <div className={'flex w-[296px] flex-col'}>
+      <div className={'flex w-full flex-col items-center gap-3'}>
+        <div className={'items-cetner flex size-25 justify-center overflow-hidden rounded-full'}>
+          <Image src={Avatar} alt={'프로필 아바타 이미지'} />
+        </div>
+        <div className={'border-outline-variant flex w-full flex-col items-center gap-4 border-b pb-4'}>
+          <h4 className={'text-h4 font-semibold'}>{'카리나'}</h4>
+          <RepositoryBadge as={'button'} label={'레포지토리 관리'} count={3} />
+        </div>
+      </div>
+
+      <div>
+        <button
+          className={
+            'text-body-large text-dark-grey-800 w-full cursor-pointer p-3 text-start font-medium hover:text-dark-grey-600'
+          }
+        >
+          {'로그아웃'}
+        </button>
+      </div>
+    </div>
+  );
+}
