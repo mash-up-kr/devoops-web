21e7157a88c9bbb274a433695172650c7f8121b7
jeongwoo903 <www8565@naver.com>
2025-07-08 19:37:09 +0900

    env: base url `NEXT_PUBLIC` 제거

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -7,7 +7,7 @@ interface IErrorResponse {
 }
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.API_BASE_URL,
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
