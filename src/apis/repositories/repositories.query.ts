c79e0f330af69ea7e4923b645b65664bfefbb44e
jeongwoo903 <www8565@naver.com>
2025-07-11 15:46:08 +0900

    feat: `useGetMyRepositoriesQuery` 추가

--- src/apis/repositories/repositories.query.ts
+++ src/apis/repositories/repositories.query.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { apiApi } from '@/__generated__/Api/Api.api';
 import { UseQueryParams } from '@/types/tanstack-query/use-query-params';
@@ -8,14 +8,26 @@ import { Parameter } from '@/types/utility/parameter';
 export const REPOSITORIES_API_QUERY_KEY = {
   GET_PULL_REQUESTS: (params?: Parameter<typeof apiApi.getRepositoryPullRequests>) =>
     ['repositories', params].filter(isNotNull),
+  GET_MY_REPOSITORIES: (params?: Parameter<typeof apiApi.getMyRepositories>) =>
+    ['repositories', params].filter(isNotNull),
 };
 
 export const useGetPullRequestsQuery = (params: UseQueryParams<typeof apiApi.getRepositoryPullRequests>) => {
   const queryKey = REPOSITORIES_API_QUERY_KEY.GET_PULL_REQUESTS(params?.variables);
 
-  return useQuery({
+  return useSuspenseQuery({
     queryKey,
     queryFn: () => apiApi.getRepositoryPullRequests(params?.variables),
+    ...params?.options,
+  });
+};
+
+export const useGetMyRepositoriesQuery = (params: UseQueryParams<typeof apiApi.getMyRepositories>) => {
+  const queryKey = REPOSITORIES_API_QUERY_KEY.GET_MY_REPOSITORIES(params?.variables);
+
+  return useSuspenseQuery({
+    queryKey,
+    queryFn: () => apiApi.getMyRepositories(params?.variables),
     ...params?.options,
   });
 };
