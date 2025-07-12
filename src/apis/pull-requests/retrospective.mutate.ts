9a9b1af5db0ff73de1c4b246cbdc67e9bf62d698
xxj15 <seojini00@naver.com>
2025-07-12 14:24:51 +0900

    feat: 회고 답변 생성용 훅 추가

--- src/apis/pull-requests/retrospective.mutate.ts
+++ src/apis/pull-requests/retrospective.mutate.ts
@@ -21,3 +21,16 @@ export async function submitRetrospectiveAnswers(user: UserType, answers: { answ
 
   return res.data;
 }
+
+export async function createRetrospectiveAnswer(questionId: number, user: UserType) {
+  try {
+    const res = await apiApi.createAnswer({
+      questionId,
+      query: { user },
+    });
+    return res.data;
+  } catch (error: any) {
+    const message = error.response?.data?.message || '회고 생성 실패';
+    throw new Error(message);
+  }
+}
