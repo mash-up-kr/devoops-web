a93a24a5e287ad9167e60195658bdcd851fa16ca
jeongwoo903 <www8565@naver.com>
2025-07-12 18:38:26 +0900

    refactor: 홈 컴포넌트 api 연결 및 구조 리팩토링

--- src/components/home/MyPR.tsx
+++ src/components/home/MyPR.tsx
@@ -1,101 +1,175 @@
+'use client';
+
+import { Suspense, useEffect, useMemo, useState } from 'react';
+
+import { RepositoryPullRequestResponseType, RepositorySummaryType } from '@/__generated__/@types';
+import {
+  useGetEntirePullRequestsQuery,
+  useGetMyRepositoriesQuery,
+  useGetPullRequestsQuery,
+} from '@/apis/repositories/repositories.query';
 import AddIcon from '@/assets/svg/add.svg';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationPrevious,
+  PaginationNext,
+  PaginationEllipsis,
+} from '@/components/common/Pagination';
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/common/Tabs';
 import Preview from '@/components/home/Preview';
 import { PRItem, PRStatus, PRContent } from '@/components/home/PRItem';
-
-const data: {
-  count: number;
-  pullRequests: {
-    id: number;
-    title: string;
-    recordStatus: 'PENDING' | 'PROGRESS' | 'DONE';
-    mergedAt: string;
-    summary: string;
-    tag: string;
-  }[];
-} = {
-  count: 20,
-  pullRequests: [
-    {
-      id: 101,
-      title: '사용자 인증 기능 추가',
-      recordStatus: 'PENDING',
-      mergedAt: '2025-06-20',
-      summary:
-        '알림 도메인의 책임을 타입 계층으로 분리해 Notification Service의 의존성과 복잡도를 낮추고, 각 알림 객체 내부로 발송 책임을 캡슐화한 리팩터링입니다.',
-      tag: 'feat',
-    },
-    {
-      id: 102,
-      title: '캐싱 미들웨어 도입',
-      recordStatus: 'PROGRESS',
-      mergedAt: '2025-06-19',
-      summary: '',
-      tag: 'feature',
-    },
-    {
-      id: 103,
-      title: 'UI 다국어 처리',
-      recordStatus: 'DONE',
-      mergedAt: '2025-06-18',
-      summary: 'UI 컴포넌트 다국어 처리를 적용해 사용자 경험을 개선',
-      tag: 'feat',
-    },
-    {
-      id: 104,
-      title: 'API Rate Limiting 추가',
-      recordStatus: 'PROGRESS',
-      mergedAt: '2025-06-17T19:50:22Z',
-      summary: '',
-      tag: 'feature',
-    },
-    {
-      id: 105,
-      title: '테스트 커버리지 확장',
-      recordStatus: 'PENDING',
-      mergedAt: '2025-06-15T07:12:01Z',
-      summary: '',
-      tag: 'feature',
-    },
-  ],
-};
+import PRPreview from '@/components/home/PRPreview';
+import PreviewSkeleton from '@/components/home/Skeleton/PreviewSkeleton';
+import { usePagination } from '@/hooks/usePagination';
 
 export default function MyPR() {
+  const [repositoryId, setRepositoryId] = useState<number>(0);
+  const [pullRequestId, setPullRequestId] = useState<number | undefined>(undefined);
+  const [page, setPage] = useState(1);
+  const pageSize = 5;
+
+  const { data: myRepositoriesData } = useGetMyRepositoriesQuery({
+    variables: {
+      data: { url: '' },
+    },
+  });
+
+  const { data: PRData } = useGetPullRequestsQuery({
+    variables: {
+      repositoryId,
+      query: {
+        size: pageSize,
+        page,
+      },
+    },
+    options: {
+      enabled: repositoryId !== 0,
+    },
+  });
+
+  const { data: entirePRData } = useGetEntirePullRequestsQuery({
+    variables: {
+      query: {
+        size: pageSize,
+        page,
+      },
+    },
+    options: {
+      enabled: repositoryId === 0,
+    },
+  });
+
+  const entireRepositoryData = {
+    id: 0,
+    name: '전체',
+    pullRequestCount: myRepositoriesData.data.repositories?.reduce((acc, repository) => {
+      return acc + (repository.pullRequestCount || 0);
+    }, 0),
+  };
+
+  const repositoryList = [entireRepositoryData, ...(myRepositoriesData.data.repositories || [])];
+  const PRList = useMemo(
+    () => (repositoryId === 0 ? entirePRData?.data.pullRequests : PRData?.data.pullRequests) || [],
+    [repositoryId, entirePRData, PRData],
+  );
+
+  useEffect(() => {
+    setPullRequestId(PRList[0]?.id);
+  }, [PRList]);
+
+  const handleTabChange = (repository: RepositorySummaryType) => {
+    if (repository.id !== undefined) {
+      setRepositoryId(repository.id);
+      setPage(1);
+    }
+  };
+
+  const handlePRItemOver = (pr: RepositoryPullRequestResponseType) => {
+    if (pr.id) {
+      setPullRequestId(pr.id);
+    }
+  };
+
+  const totalPullRequestCount =
+    repositoryId === 0
+      ? entireRepositoryData.pullRequestCount
+      : repositoryList.find((repo) => repo.id === repositoryId)?.pullRequestCount;
+
+  const totalPage = Math.ceil((totalPullRequestCount || 0) / pageSize);
+  const { pagesToShow } = usePagination({ totalPage, currentPage: page });
+
   return (
     <div>
-      <h1
-        className={`
-          text-h1 blue-tiny-left inline-block pt-2.5 pb-6 font-semibold
-        `}
-      >
-        {'내 PR'}
-      </h1>
-      <Tabs defaultValue={'전체'} className={'w-full'}>
+      <h1 className={`text-h1 blue-tiny-left inline-block pt-2.5 pb-6 font-semibold`}>{'내 PR'}</h1>
+      <Tabs defaultValue={repositoryList[0]?.name || '전체'} className={'w-full'}>
         <TabsList>
-          <TabsTrigger value={'전체'}>{'전체'}</TabsTrigger>
-          <TabsTrigger value={'레포1'}>{'레포1'}</TabsTrigger>
+          {repositoryList.map((repository) => (
+            <TabsTrigger key={repository.id} value={repository.name || ''} onClick={() => handleTabChange(repository)}>
+              {repository.name}
+            </TabsTrigger>
+          ))}
           <AddIcon />
         </TabsList>
-        <TabsContent value={'전체'} className={'flex'}>
-          <div
-            className={`
-              border-dark-grey-100 flex flex-1 flex-col gap-5 border-e-1 py-4
-              pe-8
-            `}
-          >
-            {data.pullRequests.map((pr) => (
-              <PRItem key={pr.id}>
-                <PRStatus status={pr.recordStatus} />
-                <PRContent content={pr.title} label={pr.tag} />
-              </PRItem>
-            ))}
-          </div>
-          <Preview content={data.pullRequests[0].summary} />
-        </TabsContent>
-        <TabsContent value={'레포1'} className={'p-4'}>
-          <h3 className={'mb-4 text-lg font-medium'}>{'Password Settings'}</h3>
-          <p className={'text-gray-300'}>{'Change your password here.'}</p>
-        </TabsContent>
+
+        {repositoryList.map((repository) => (
+          <TabsContent key={repository.id} value={repository.name || ''} className={'flex'}>
+            <div className={`border-dark-grey-100 flex flex-1 flex-col gap-5 border-e-1 py-4 pe-8`}>
+              {PRList.map((pr) => (
+                <PRItem key={pr.id} onMouseOver={() => handlePRItemOver(pr)}>
+                  <PRStatus status={pr.recordStatus} />
+                  <PRContent content={pr.title} label={pr.tag || ''} />
+                </PRItem>
+              ))}
+
+              <Pagination>
+                <PaginationContent>
+                  <PaginationItem>
+                    <PaginationPrevious
+                      className={'flex size-10 items-center justify-center rounded-full'}
+                      onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+                    />
+                  </PaginationItem>
+
+                  {pagesToShow.map((p, idx) => {
+                    if (p === 'ellipsis') {
+                      const key = idx === 0 ? 'ellipsis-left' : 'ellipsis-right';
+                      return (
+                        <PaginationItem key={key}>
+                          <PaginationEllipsis />
+                        </PaginationItem>
+                      );
+                    }
+
+                    return (
+                      <PaginationItem key={`page-${p}`}>
+                        <PaginationLink
+                          isActive={page === p}
+                          onClick={() => setPage(p)}
+                          className={`text-body-medium flex size-10 items-center justify-center rounded-full transition-colors duration-100 ease-out ${page === p ? 'bg-dark-grey-50 font-medium' : 'font-regular'}`}
+                        >
+                          {p}
+                        </PaginationLink>
+                      </PaginationItem>
+                    );
+                  })}
+
+                  <PaginationItem>
+                    <PaginationNext
+                      className={'flex size-10 items-center justify-center rounded-full'}
+                      onClick={() => setPage((prev) => Math.min(prev + 1, totalPage))}
+                    />
+                  </PaginationItem>
+                </PaginationContent>
+              </Pagination>
+            </div>
+            <Suspense fallback={<PreviewSkeleton />}>
+              {pullRequestId ? <PRPreview pullRequestId={pullRequestId} /> : <Preview />}
+            </Suspense>
+          </TabsContent>
+        ))}
       </Tabs>
     </div>
   );
