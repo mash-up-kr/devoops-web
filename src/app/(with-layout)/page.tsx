cea234ad2be5b3fceea666b2f1f7e400c1812f78
jeongwoo903 <www8565@naver.com>
2025-07-12 18:27:00 +0900

    feat: `PreviewSkeleton` 스켈레톤 컴포넌트 제작 및 예외 처리 구현
    * 해당 부분 비 효율적인 구조 리팩토링 필요

--- src/app/(with-layout)/page.tsx
+++ src/app/(with-layout)/page.tsx
@@ -1,9 +1,14 @@
+import { Suspense } from 'react';
+
 import MyPR from '@/components/home/MyPR';
+import MyPRSkeleton from '@/components/home/Skeleton/MyPRSkeleton';
 
 export default function Home() {
   return (
     <div className={'mx-auto max-w-[1200px] px-[40px]'}>
-      <MyPR />
+      <Suspense fallback={<MyPRSkeleton />}>
+        <MyPR />
+      </Suspense>
     </div>
   );
 }
--- src/components/home/Preview.tsx
+++ src/components/home/Preview.tsx
@@ -1,6 +1,7 @@
 import { PullRequestReadResponseType } from '@/__generated__/@types';
 import AISummary from '@/components/home/AISummary';
 import QuestionPreview from '@/components/home/QuestionPreview';
+import PreviewSkeleton from '@/components/home/Skeleton/PreviewSkeleton';
 
 interface PreviewProps {
   data?: PullRequestReadResponseType;
@@ -8,16 +9,11 @@ interface PreviewProps {
 
 export default function Preview({ data }: PreviewProps) {
   if (!data) {
-    return (
-      <div className={'text-dark-grey-500 mx-8 my-5 flex max-w-[438px] flex-col items-center justify-center pt-5'}>
-        <h5 className={'text-h5 mb-[23px] font-medium'}>{'미리보기'}</h5>
-        <p>{'미리 볼 PR 항목에 마우스를 올려주세요.'}</p>
-      </div>
-    );
+    return <PreviewSkeleton />;
   }
 
   return (
-    <div className={'mx-8 my-5 flex max-w-[438px] flex-col pt-5'}>
+    <div className={'mx-8 my-5 flex flex-col pt-5 md:w-[300px] xl:w-[438px]'}>
       <h5 className={'text-h5 mb-[23px] font-medium'}>{'미리보기'}</h5>
       <div className={'border-dark-grey-100 border-b pb-7'}>
         <AISummary contents={data.summary} />
