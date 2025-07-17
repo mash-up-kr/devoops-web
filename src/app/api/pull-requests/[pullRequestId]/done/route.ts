87714832196efc308eb1ce0fdd97efc3ac1c4953
xxj15 <seojini00@naver.com>
2025-07-18 01:46:18 +0900

    fix: 테스트용 route.ts 삭제

--- src/app/api/pull-requests/[pullRequestId]/done/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function PATCH(req: Request, { params }: { params: { pullRequestId: string } }) {
-  const { pullRequestId } = params;
-
-  return NextResponse.json({ message: `PR ${pullRequestId} 회고 완료됨` }, { status: 200 });
-}
