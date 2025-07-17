2313bcc750fabda309f261a5b40b908326b055db
jeongwoo903 <www8565@naver.com>
2025-07-17 17:24:25 +0900

    fix: 토큰이 없는 경우 상태에러에 대응 하지 못하는 부분 수정

--- src/app/(with-layout)/page.tsx
+++ src/app/(with-layout)/page.tsx
@@ -1,12 +1,19 @@
+import { RepositorySummaryType } from '@/__generated__/@types';
 import { apiApi } from '@/__generated__/Api/Api.api';
 import MyPR from '@/components/home/MyPR';
 
 export default async function Home() {
-  const { data: myReposRes } = await apiApi.getMyRepositories({ data: { url: '' } });
+  let initialRepositoryList: RepositorySummaryType[] = [];
 
-  const repos = myReposRes.repositories ?? [];
-  const totalCount = repos.reduce((sum, r) => sum + (r.pullRequestCount || 0), 0);
-  const initialRepositoryList = [{ id: 0, name: '전체', pullRequestCount: totalCount }, ...repos];
+  try {
+    const { data: myReposRes } = await apiApi.getMyRepositories({ data: { url: '' } });
+    const repos = myReposRes.repositories ?? [];
+
+    const totalCount = repos.reduce((sum, r) => sum + (r.pullRequestCount || 0), 0);
+    initialRepositoryList = [{ id: 0, name: '전체', pullRequestCount: totalCount }, ...repos];
+  } catch {
+    initialRepositoryList = [];
+  }
 
   return (
     <div className={'mx-auto max-w-[1200px] px-[40px]'}>
