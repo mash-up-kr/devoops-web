8e1b2e5033f16a8a1e99599ceb1c35e4baed1e47
jeongwoo903 <www8565@naver.com>
2025-07-19 00:18:59 +0900

    fix: 내용 수정

--- src/components/landing/Section/FeatureSection.tsx
+++ src/components/landing/Section/FeatureSection.tsx
@@ -45,27 +45,32 @@ export default function FeatureSection() {
             </div>
           </FeatureCard>
           <FeatureCard image={LandingSymbol2}>
-            <h3 className={'text-h3'}>{'레포지토리 연동'}</h3>
+            <h3 className={'text-h3'}>{'회고를 돕는 AI'}</h3>
             <div>
               <p className={'text-h5'}>
                 <span className={'blue-tiny-right bg-clip-text font-semibold text-transparent'}>
-                  {'GitHub PR을 자동으로 가져'}
+                  {'PR 내용을 요약'}
                 </span>
-                {'와요.'}
+                {'해주고,'}
               </p>
-              <p>{'회고할 작업을 한눈에 확인할 수 있어요.'}</p>
+              <p className={'text-h5'}>
+                <span className={'blue-tiny-right bg-clip-text font-semibold text-transparent'}>
+                  {'AI가 고민을 꺼내줄 질문'}
+                </span>
+                {'도 던져줘요.'}
+              </p>
             </div>
           </FeatureCard>
           <FeatureCard image={LandingSymbol3}>
-            <h3 className={'text-h3'}>{'레포지토리 연동'}</h3>
+            <h3 className={'text-h3'}>{'질문만 답해서 완료!'}</h3>
             <div>
+              <p className={'text-h5'}>{'부담 없이 한 줄씩 적다 보면'}</p>
               <p className={'text-h5'}>
                 <span className={'blue-tiny-right bg-clip-text font-semibold text-transparent'}>
-                  {'GitHub PR을 자동으로 가져'}
+                  {'중요한 맥락이 자연스럽게 기록'}
                 </span>
-                {'와요.'}
+                {'돼요.'}
               </p>
-              <p>{'회고할 작업을 한눈에 확인할 수 있어요.'}</p>
             </div>
           </FeatureCard>
         </div>
