fdfbcd281ee142a4bd65c918858f273ed1f2b7d7
jeongwoo903 <www8565@naver.com>
2025-07-18 16:21:10 +0900

    feat: landing 페이지 제작

---
+++ src/components/landing/GradientBottomLayer.tsx
@@ -0,0 +1,7 @@
+export default function GradientBottomLayer() {
+  return (
+    <div
+      className={'absolute bottom-0 h-[200px] w-full flex-shrink-0 [background:var(--color-gradient-bottom-layer)]'}
+    />
+  );
+}
