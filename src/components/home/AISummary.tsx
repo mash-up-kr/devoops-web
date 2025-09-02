f9569a9d434f2748342009050fdce9911e919086
jeongwoo903 <www8565@naver.com>
2025-09-02 13:34:05 +0900

    style: 텍스트 break-all 처리

--- src/components/home/AISummary.tsx
+++ src/components/home/AISummary.tsx
@@ -6,13 +6,7 @@ export default function AISummary({ contents }: AISummaryProps) {
   return (
     <div className={'flex flex-col gap-2'}>
       <div className={'text-body-small text-dark-grey-500 font-medium'}>{'AI 요약'}</div>
-      <div
-        className={`
-          text-body-medium font-regular text-dark-grey-700
-        `}
-      >
-        {contents}
-      </div>
+      <div className={`text-body-medium font-regular text-dark-grey-700 break-all`}>{contents}</div>
     </div>
   );
 }
