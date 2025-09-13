fdd1fd74e43443ea0919e5c445abc07915e3ffa7
jeongwoo903 <www8565@naver.com>
2025-09-14 01:00:21 +0900

    refactor: 라벨 태그에 대한 색상 로직 수정

--- src/utils/getTagColor.ts
+++ src/utils/getTagColor.ts
@@ -1,10 +1,31 @@
+import type { VariantProps } from 'class-variance-authority';
+
+import { dotVariants } from '@/components/common/Tag';
+
+type TagKeys = 'feat' | 'refactor' | 'bug' | 'chore' | 'style' | 'test' | 'pref' | 'deploy' | 'fix' | 'none';
+
+const tagColors: Record<TagKeys, VariantProps<typeof dotVariants>['dotColor']> = {
+  feat: 'skyblue',
+  refactor: 'violet',
+  bug: 'red',
+  chore: 'olive',
+  style: 'orange',
+  test: 'blue',
+  pref: 'yellow',
+  deploy: 'lime',
+  fix: 'rose',
+  none: 'gray',
+};
+
+const findMatchingTag = (tagName: string) => {
+  const checkTag = Object.keys(tagColors).find((tag) => {
+    return new RegExp(tag, 'i').test(tagName);
+  });
+
+  return checkTag || 'none';
+};
+
 export const getTagColor = (tagName: string) => {
-  if (tagName.includes('feat')) return 'skyblue';
-  if (tagName.includes('refactor')) return 'violet';
-  if (tagName.includes('bug')) return 'red';
-  if (tagName.includes('chore')) return 'olive';
-  if (tagName.includes('style')) return 'orange';
-  if (tagName.includes('test')) return 'blue';
-  if (tagName.includes('pref')) return 'yellow';
-  return 'gray';
+  const foundTag = findMatchingTag(tagName);
+  return tagColors[foundTag as TagKeys];
 };
