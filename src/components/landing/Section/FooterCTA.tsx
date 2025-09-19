4593a76b97681b0a866fa01f8e55d7e5a6528cd3
jeongwoo903 <www8565@naver.com>
2025-09-20 00:35:53 +0900

    style: 랜딩 페이지 여백 값 추가 - media query 값 파편화를 막기 위한 파일 관리

--- src/components/landing/Section/FooterCTA.tsx
+++ src/components/landing/Section/FooterCTA.tsx
@@ -6,7 +6,7 @@ interface FooterCTAProps {
 
 export default function FooterCTA({ action }: FooterCTAProps) {
   return (
-    <div>
+    <div className={'max-lg:px-landing-mobile-padding'}>
       <div className={'mx-auto mb-[48px] flex items-center justify-center gap-8 py-[96px]'}>
         <h1 className={'text-h1 blue-tiny-right inline-block leading-[58px] font-bold'}>
           {'복잡한 회고, Devoops가 슬쩍 정리해줄게요.'}
