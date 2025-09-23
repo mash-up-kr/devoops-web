305379ba36ee1719cd166a695e267154ff5ae9d1
jeongwoo903 <www8565@naver.com>
2025-09-23 14:44:01 +0900

    chore: 불필요한 console.log 삭제

--- src/app/(with-layout)/page.tsx
+++ src/app/(with-layout)/page.tsx
@@ -10,7 +10,6 @@ export default async function Home() {
     const { data: myRepositories } = await apiApi.getMyRepositories();
     const repositoriesData = myRepositories.repositories ?? [];
     const sortedRepositoriesByTracking = sortRepositoriesByTracking(repositoriesData);
-    console.log(sortedRepositoriesByTracking);
 
     const totalCount = repositoriesData.reduce((sum, r) => sum + (r.pullRequestCount || 0), 0);
     initialRepositoryList = [{ id: 0, name: '전체', pullRequestCount: totalCount }, ...sortedRepositoriesByTracking];
