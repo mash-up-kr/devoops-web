dd22bdfe2be77d2ac4199ff9f49a2afa973d5d91
jeongwoo903 <www8565@naver.com>
2025-09-25 00:30:29 +0900

    refactor: prefetch를 통한 Server Action + useSuspense Query 사용

--- src/components/home/MyPR.tsx
+++ src/components/home/MyPR.tsx
@@ -1,48 +1,66 @@
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+
+import { RepositorySummaryType } from '@/__generated__/@types';
 import { apiApi } from '@/__generated__/Api/Api.api';
+import { REPOSITORIES_API_QUERY_KEY } from '@/apis/repositories/repositories.query';
 import { RepolinkButton, RepolinkModal } from '@/components/common/Modal/RepolinkModal';
 import Spacing from '@/components/common/Spacing';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/common/Tabs';
 import Overview from '@/components/home/Overview';
+import { RepositoryTrigger } from '@/components/home/RepositoryTrigger';
+import { ITEMS_PER_PAGE } from '@/constants/domain';
 import { sortRepositoriesByTracking } from '@/utils/sortRepositoriesByTracking';
 
-export async function MyPR() {
-  const { data: myRepositories } = await apiApi.getMyRepositories();
+export const prepareRepositories = (repositories: RepositorySummaryType[]) => {
+  const sortedRepositoriesByTracking = sortRepositoriesByTracking(repositories);
+  const totalCount = repositories.reduce((total, repository) => total + (repository.pullRequestCount || 0), 0);
 
-  const repositoriesData = myRepositories.repositories ?? [];
-  const sortedRepositoriesByTracking = sortRepositoriesByTracking(repositoriesData);
-  const totalCount = repositoriesData.reduce((total, repository) => total + (repository.pullRequestCount || 0), 0);
-  const initialRepositoryList = [
-    { id: 0, name: '전체', pullRequestCount: totalCount },
-    ...sortedRepositoriesByTracking,
-  ];
+  return [{ id: 0, name: '전체', pullRequestCount: totalCount }, ...sortedRepositoriesByTracking];
+};
+
+export async function MyPR() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: REPOSITORIES_API_QUERY_KEY.GET_REPOSITORY_ENTIRE_PULL_REQUESTS({
+      query: { page: 0, size: ITEMS_PER_PAGE },
+    }),
+    queryFn: async () => {
+      const res = await apiApi.getRepositoryEntirePullRequests({
+        query: { page: 0, size: ITEMS_PER_PAGE },
+      });
+
+      return { data: res.data };
+    },
+  });
+
+  const { data: myRepositories } = await apiApi.getMyRepositories();
+  const repositories = myRepositories.repositories ?? [];
+  const initRepositories = prepareRepositories(repositories);
 
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <div>
         <h1 className={'text-h1 blue-tiny-left inline-block pt-2.5 pb-6 font-semibold'}>{'내 PR'}</h1>
-        <Tabs defaultValue={initialRepositoryList[0]?.name || ''}>
+        <Tabs defaultValue={initRepositories[0]?.name || ''}>
           <div className={'border-dark-grey-100 border-b-1'}>
             <TabsList aria-label={'내 PR 목록'}>
-              {initialRepositoryList.map((repository) => (
+              {repositories.map((repository) => (
                 <TabsTrigger key={repository.id} value={repository.name || ''}>
-                  <p>{repository.name}</p>
-                  <p className={'text-dark-grey-500'}>{repository.pullRequestCount}</p>
+                  <RepositoryTrigger repository={repository} />
                 </TabsTrigger>
               ))}
               <RepolinkButton action={'OPEN'} />
             </TabsList>
           </div>
-          <div>
-            {initialRepositoryList.map((repository) => (
-              <TabsContent key={repository.id} value={repository.name || ''}>
-                <Overview repository={repository} />
-              </TabsContent>
-            ))}
-          </div>
+          {initRepositories.map((repository) => (
+            <TabsContent key={repository.id} value={repository.name || ''}>
+              <Overview repository={repository} />
+            </TabsContent>
+          ))}
         </Tabs>
         <Spacing size={400} />
       </div>
       <RepolinkModal defaultOpen={false} isOutsideClickClose button={null} />
-    </>
+    </HydrationBoundary>
   );
 }
