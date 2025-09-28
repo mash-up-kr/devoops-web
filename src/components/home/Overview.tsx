cec7de81e3ed72e2788d53ebedae5552d0746259
jeongwoo903 <www8565@naver.com>
2025-09-29 03:59:06 +0900

    refactor: preview 컴포넌트 경로 변경에 따른 수정

--- src/components/home/Overview.tsx
+++ src/components/home/Overview.tsx
@@ -3,13 +3,12 @@
 import { Suspense, useState } from 'react';
 
 import { RepositoryPullRequestResponseType, RepositorySummaryType } from '@/__generated__/@types';
-import Preview from '@/components/home/Preview';
+import { Preview } from '@/components/home/Preview';
 import { EachPRFetcher } from '@/components/home/PRFetcher/EachPRFetcher';
 import { EntirePRFetcher } from '@/components/home/PRFetcher/EntirePRFetcher';
 import { PRList } from '@/components/home/PRList';
-import PreviewSkeleton from '@/components/home/Skeleton/PreviewSkeleton';
 import PRListSkeleton from '@/components/home/Skeleton/PRListSkeleton';
-import { ITEMS_PER_PAGE } from '@/constants/domain';
+import { ITEMS_PER_PAGE, TOTAL_TABS } from '@/constants/domain';
 
 export type ActiveCategoryIndexesType = { [key: number]: number };
 
@@ -17,12 +16,12 @@ interface OverviewProps {
   repository: RepositorySummaryType;
 }
 
-export default function Overview({ repository }: OverviewProps) {
-  const [pullRequestId, setPullRequestId] = useState<number | undefined>(0);
+export function Overview({ repository }: OverviewProps) {
+  const [pullRequestId, setPullRequestId] = useState<number | undefined>(undefined);
   const [currentPage, setCurrentPage] = useState<number>(0);
   const [activeCategoryIndexes, setActiveCategoryIndexes] = useState<ActiveCategoryIndexesType>({});
 
-  const PRFetcher = repository.id ? EachPRFetcher : EntirePRFetcher;
+  const PRFetcher = repository.id === TOTAL_TABS.ID ? EntirePRFetcher : EachPRFetcher;
   const totalPage = Math.ceil((repository.pullRequestCount || 0) / ITEMS_PER_PAGE);
 
   return (
@@ -43,17 +42,12 @@ export default function Overview({ repository }: OverviewProps) {
           )}
         />
       </Suspense>
-      <Suspense fallback={<PreviewSkeleton />}>
-        {pullRequestId && (
-          <Preview
-            pullRequestId={pullRequestId}
-            activeCategoryIndex={activeCategoryIndexes[pullRequestId]}
-            setActiveCategoryIndex={(index) =>
-              setActiveCategoryIndexes((prev) => ({ ...prev, [pullRequestId]: index }))
-            }
-          />
-        )}
-      </Suspense>
+      <Preview
+        prCount={repository.pullRequestCount ?? 0}
+        pullRequestId={pullRequestId}
+        activeCategoryIndexes={activeCategoryIndexes}
+        activeCategoryIndexesAction={setActiveCategoryIndexes}
+      />
     </div>
   );
 }
