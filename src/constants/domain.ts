9cb99096d648d719b480ec129fd6cd0869fff3e8
jeongwoo903 <www8565@naver.com>
2025-09-29 04:20:37 +0900

    style: min, max 값 적용

--- src/constants/domain.ts
+++ src/constants/domain.ts
@@ -1,4 +1,10 @@
 export const ITEMS_PER_PAGE = 5;
 export const NO_PR_IN_REPOSITORY_ID = 20010903;
+export const CONTENT_TRUNCATE_THRESHOLD = 95;
 
 export const REDIRECT_NO_PERMISSION_URL = 'https://github.com/settings/connections/applications/Ov23lid4k1ItHTh8UuNY';
+
+export const TOTAL_TABS = {
+  ID: 1231241241,
+  NAME: '전체',
+};
