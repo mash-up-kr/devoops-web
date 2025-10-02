ac4d3cda7424102e372feaf77e25e8733873a03c
jeongwoo903 <www8565@naver.com>
2025-10-03 00:54:14 +0900

    chore: rETROSPECTIVE 동적 라우팅에 대한 undefined 값 허용

--- src/components/home/PRList.tsx
+++ src/components/home/PRList.tsx
@@ -77,7 +77,7 @@ export function PRList({
   return (
     <div className={'max-w-pr-item border-dark-grey-100 flex flex-1 flex-col gap-5 border-e-1 py-4 pe-8'}>
       {prListData.map((pr) => (
-        <Link href={ROUTES.PAGE.RETROSPECTIVE(pr.id || 0)} key={pr.id}>
+        <Link href={ROUTES.PAGE.RETROSPECTIVE(pr.id)} key={pr.id}>
           <PRItem onMouseOver={() => handlePRItemOver(pr)}>
             <PRStatus status={pr.recordStatus} />
             <PRContent content={pr.title} label={pr.tag || 'none'} />
